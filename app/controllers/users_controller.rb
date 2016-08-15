78b293fb1e43eed4f59c956a5d8df2574b00f867
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-08-15 17:13:00 +0900

    @user = User.find(params[:is])を before_actionで簡潔にする。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,20 @@
 class UsersController < ApplicationController
+  before_action: :set_user,only:[:show,:edit,:update]
   def show
-    @user = User.find(params[:id])
   end
   def edit
-    @user = User.find(params[:id])
   end
   def update
-    user = User.find(params[:id])
-    user.update(update_params)
+    @user.update(update_params)
     redirect_to root_path
   end
 
   private
   def update_params
     params.permit(:username,:email,:password,:member,:profile,:works)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
