e79ebb93151641e10c5c2ea59b231cb9c1627f9e
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-08-15 13:09:00 +0900

    show-user-mypage 不要ファイル削除 userコントローラーの 変数を減らす。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,5 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @member = user.member
-    @works = user.works
-    @profile = user.profile
+    @user = User.find(params[:id])
   end
 end
