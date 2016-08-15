863bbcc0152c055308e3335ee6259ef0b8fcf9a8
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-08-15 16:46:18 +0900

    ユーザー情報の編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    user = User.find(params[:id])
+    user.update(update_params)
+    redirect_to root_path
+  end
+
+  private
+  def update_params
+    params.permit(:username,:email,:password,:member,:profile,:works)
+  end
 end
