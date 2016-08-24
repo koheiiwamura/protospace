aed3351e6769b8eb8158b9030eeb18aa5e29cbc3
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-08-24 13:32:41 +0900

    不要ファイル、変数削除、メソッド作成

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,13 +2,17 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-    @prototype = Prototype.find(params[:prototype_id])
+    set_prototype
   end
 
   def destroy
     @like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     @like.destroy
-    @likes = Like.where(prototype_id: params[:prototype_id])
+    set_prototype
+  end
+
+  private
+  def set_prototype
     @prototype = Prototype.find(params[:prototype_id])
   end
 end
