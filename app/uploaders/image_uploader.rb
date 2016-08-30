3d056041600be05e057ec6104a3e25ae3b8b1a36
岩村康平 <hershey-s-.7s2.w1@docomo.ne.jp>
2016-08-30 17:55:31 +0900

    S3導入,Turbolinksの改善(Javascriptの読み込みが遅いため)

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -7,8 +7,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -17,12 +16,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url
-    # For Rails 3.1+ asset pipeline compatibility:
-    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-    "/images/fallback/" + [version_name, "sample.png"].compact.join('_')
-    # "default.png"
-  end
+  # def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:
+  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #   "/images/fallback/" + [version_name, "sample.png"].compact.join('_')
+  #   # "default.png"
+  # end
 
 #   # Process files as they are uploaded:
 #   # process :scale => [200, 300]
